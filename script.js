1520d327f67fb52d6139f00b2873a390c9bd352e
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-01-27 16:59:36 +0100

    experiment

--- script.js
+++ script.js
@@ -5,54 +5,116 @@ const options = {
     "X-RapidAPI-Host": "similar-web.p.rapidapi.com",
   },
 };
+let domains = ["google.com", "avatarai.me"];
+const dataPoints = [];
+const testData = [12, 19, 3, 5, 2, 3];
+const testLabels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
 
-const getDomains = async (domain) => {
+const fetchDomains = async () => {
   try {
-    const res = await fetch(
-      `https://similar-web.p.rapidapi.com/get-analysis?domain=${domain}`,
-      options
-    );
+    for (let domain of domains) {
+      const res = await fetch(
+        `https://similar-web.p.rapidapi.com/get-analysis?domain=${domain}`,
+        options
+      );
 
-    if (res.ok) {
-      let data = await res.json();
-      let visits = data.EstimatedMonthlyVisits;
-      let months = [];
-      let visitsPerMonth = [];
-
-      let keys = Object.keys(visits);
-      let values = Object.values(visits);
-
-      keys.forEach((key) => {
-        months.push(key);
-        console.log(key);
-      });
-      values.forEach((value) => {
-        visitsPerMonth.push(value);
-        console.log(value);
-      });
-      console.log(months);
-      console.log(visitsPerMonth);
-      renderChart(months, visitsPerMonth);
+      if (res.ok) {
+        let data = await res.json();
+        dataPoints.push(data);
+      }
     }
   } catch (error) {}
+  console.log(dataPoints);
+  console.log("completed data fetch");
+  prepareForRender();
 };
 
-function renderChart(labels, data) {
-  var ctx = document.getElementById("myChart").getContext("2d");
+const prepareForRender = () => {
+  console.log("teest", dataPoints.length);
+
+  for (let dataPoint of dataPoints) {
+    let chartContainerNode = document.getElementById("chart-container-outer");
+    chartContainerNode.innerHTML += `<div class="container" id="div${dataPoint.SiteName}"></div>`;
+    let visits = dataPoint.EstimatedMonthlyVisits;
+    let months = [];
+    let visitsPerMonth = [];
+
+    let keys = Object.keys(visits);
+    let values = Object.values(visits);
+
+    keys.forEach((key) => {
+      months.push(key);
+      console.log(key);
+    });
+    values.forEach((value) => {
+      visitsPerMonth.push(value);
+      console.log(value);
+    });
+    console.log(months);
+    console.log(visitsPerMonth);
+    renderChart(months, visitsPerMonth, dataPoint.SiteName);
+  }
+};
+
+function renderChart(labels, data, domain) {
+  function makeid(length) {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+  let ctx = makeid(5);
+  // let chartContainerInnerNode = document.getElementById(`div${domain}`);
+  // chartContainerInnerNode.innerHTML = `<canvas id="${domain}"></canvas>`;
+
+  let chartContainerInnerNode = document.getElementById(`div${domain}`);
+  let canvas = document.createElement("canvas");
+  canvas.id = domain;
+  chartContainerInnerNode.appendChild(canvas);
+
+  ctx = document.getElementById(domain).getContext("2d");
   new Chart(ctx, {
-    type: "line",
+    type: "bar",
     data: {
       labels: labels,
       datasets: [
         {
-          label: "This week",
+          label: "# of Votes",
           data: data,
+          borderWidth: 2,
         },
       ],
     },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
   });
+
+  // new Chart(ctx, {
+  //   type: "line",
+  //   data: {
+  //     labels: labels,
+  //     datasets: [
+  //       {
+  //         label: `${domain}`,
+  //         data: data,
+  //       },
+  //     ],
+  //   },
+  // });
+  console.log("rendered a chart");
 }
 
 window.onload = () => {
-  getDomains("openai.com");
+  fetchDomains();
 };
